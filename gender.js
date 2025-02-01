7677883c0c6b6231403e6dbcf4b96a2d293f9b92
Fares <fareshatahet491@gmail.com>
2025-02-01 04:53:24 +0300

    Gender 1.1(ribbon & mustache)

--- gender.js
+++ gender.js
@@ -2,6 +2,7 @@ const genderSymbol = document.querySelector(".gender-symbol");
 const genderArrow = document.querySelector(".gender-arrow");
 const arrowHead = document.querySelector(".arrow-head");
 const cross = document.querySelector(".female-cross");
+const genderImage = document.querySelector(".gender-image");
 const radioMale = document.getElementById("male");
 const radioFemale = document.getElementById("female");
 
@@ -27,7 +28,16 @@ function updateGenderSymbol() {
       background: "transparent",
       ease: "ease",
     });
+    gsap.fromTo(genderImage, { scale: 0.6 }, { scale: 0.9 });
+    gsap.to(genderImage, {
+      scale: 0.9,
+      rotate: 0,
+      x: 0,
+      y: 10,
+    });
     arrowHead.style.borderBottomColor = "#006aff";
+    genderImage.style.visibility = "visible";
+    genderImage.src = "Assets/images/moustache.png";
   } else if (radioFemale.checked) {
     genderSymbol.classList.add("female-color");
     genderSymbol.classList.remove("male-color");
@@ -51,7 +61,15 @@ function updateGenderSymbol() {
       background: "#d400ff",
       ease: "ease",
     });
+    genderImage.style.visibility = "visible";
+    genderImage.src = "Assets/images/ribbon.png";
     arrowHead.style.borderBottomColor = "transparent";
+    gsap.to(genderImage, {
+      scale: 0.6,
+      rotate: 25,
+      x: 20,
+      y: -30,
+    });
   }
 }
 radioMale.addEventListener("change", updateGenderSymbol);
